926e7cb82ddca0d69ee5207f669827c2939be6b8
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2026-05-22 15:55:53 +0530

    use redis cache

--- api/cache.js
+++ api/cache.js
@@ -1,11 +1,15 @@
-const NodeCache = require("node-cache");
+const Keyv = require("keyv").default;
+const KeyvRedis = require("@keyv/redis").default;
 const crypto = require("crypto");
 const stringify = require("fast-json-stable-stringify");
 const { Duration } = require("luxon");
 
 const CACHE_TTL = Duration.fromObject({ days: 7 });
 
-const cache = new NodeCache({ stdTTL: CACHE_TTL.as("seconds") });
+const cache = new Keyv({
+  store: new KeyvRedis(process.env.REDIS_URL),
+  ttl: CACHE_TTL.as("milliseconds"),
+});
 
 const cached = (fn) => {
   return async (...args) => {
@@ -14,13 +18,13 @@ const cached = (fn) => {
       .update(`${fn.name}:${stringify(args)}`)
       .digest("hex");
 
-    const cachedValue = cache.get(key);
+    const cachedValue = await cache.get(key);
     if (cachedValue !== undefined) {
       return cachedValue;
     }
 
     const result = await fn(...args);
-    cache.set(key, result);
+    await cache.set(key, result);
     return result;
   };
 };
