6fa3930db4b3ee7161d5eff08bd7d5c918d29ed6
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2026-05-22 08:42:35 +0530

    use sha256 hash of args for stable cache key

--- api/cache.js
+++ api/cache.js
@@ -1,10 +1,15 @@
 const NodeCache = require("node-cache");
+const crypto = require("crypto");
+const stringify = require("fast-json-stable-stringify");
 
 const cache = new NodeCache({ stdTTL: 604800 });
 
 const cached = (fn) => {
   return async (...args) => {
-    const key = `${fn.name}:${args.join(":")}`;
+    const key = crypto
+      .createHash("sha256")
+      .update(`${fn.name}:${stringify(args)}`)
+      .digest("hex");
 
     const cachedValue = cache.get(key);
     if (cachedValue !== undefined) {
