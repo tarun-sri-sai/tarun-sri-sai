3bbc52087c999b8626f93f5f5a2f3ac3097119cb
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2026-06-30 23:30:38 +0530

    implement blog layout

--- apps/web/app/blog/[slug]/page.jsx
+++ apps/web/app/blog/[slug]/page.jsx
@@ -1,7 +1,33 @@
 import { notFound } from "next/navigation";
+import { getBlog } from "@/lib/db/blog";
+import DOMPurify from "isomorphic-dompurify";
 
-const BlogPostPage = async () => {
-  notFound();
+const BlogPostPage = async ({ params }) => {
+  const { slug } = await params;
+
+  try {
+    const blogResult = await getBlog(slug);
+    const columnMap = Object.fromEntries(
+      blogResult.columns.map((v, i) => [v, i]),
+    );
+
+    const blogRow = blogResult.rows[0];
+    const content = DOMPurify.sanitize(blogRow[columnMap["content"]]);
+
+    return (
+      <>
+        <header>
+          <h1>{blogRow[columnMap["title"]]}</h1>
+        </header>
+        <main dangerouslySetInnerHTML={{ __html: content }} />
+        <footer>
+          <p>{`Last edited on: ${new Date(blogRow[columnMap["created_at"]])}`}</p>
+        </footer>
+      </>
+    );
+  } catch (error) {
+    notFound();
+  }
 };
 
 export default BlogPostPage;
--- apps/web/app/blog/page.jsx
+++ apps/web/app/blog/page.jsx
@@ -1,5 +1,40 @@
-const BlogPage = () => {
-  return <main>Hey</main>;
+import Link from "next/link";
+import { getRecentBlogs } from "@/lib/db/blog";
+
+const BlogPage = async () => {
+  const recentBlogsResult = await getRecentBlogs();
+  const columnMap = Object.fromEntries(
+    recentBlogsResult.columns.map((v, i) => [v, i]),
+  );
+
+  return (
+    <>
+      <header>
+        <h1>Welcome to my blog!</h1>
+      </header>
+      <main>
+        <section>
+          <h2>Recent Blogs</h2>
+
+          <ul>
+            {[...recentBlogsResult.rows]
+              .sort(
+                (a, b) =>
+                  new Date(b[columnMap["created_at"]]) -
+                  new Date(a[columnMap["created_at"]]),
+              )
+              .map((r) => (
+                <li key={r[columnMap["slug"]]}>
+                  <Link href={`/blog/${r[columnMap["slug"]]}`}>
+                    {r[columnMap["title"]]}
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default BlogPage;
